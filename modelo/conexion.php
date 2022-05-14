852997dec1fc15b6e3f3e6a5b5e87a779aa38d97
Damian <damian.contreras036@gmail.com>
2022-05-14 03:00:54 -0400

    Base de proyecto HNet

---
+++ modelo/conexion.php
@@ -0,0 +1,41 @@
+<?php
+class Database{
+    private $db;
+    private $sentence;
+    public function __construct() {
+        $host = 'localhost';
+        $u = 'root';
+        $pw = '';
+        $database = 'evfinal';
+        $this->db = new mysqli($host, $u, $pw, $database);
+        $this->sentence = $this->db->stmt_init();
+    }
+
+    public function getAll($sql) {
+        if ($this->sentence->prepare($sql)) {
+            $lista = $this->db->query($sql);
+            return $lista->fetch_all(MYSQLI_ASSOC);
+            $this->sentence->close();
+        }
+        $this->db->close();
+        return null;
+    }
+
+    public function consult($sql) {
+        if ($this->sentence->prepare($sql)) {
+            $lista = $this->db->query($sql);
+            $this->sentence->close();
+            return $lista->fetch_assoc();
+        }
+
+        $this->db->close();
+
+        return null;
+    }
+    public function execute($sql) {
+        $res = $this->db->query($sql);
+        $this->db->close();
+        return $res;
+    }
+}
+?>
